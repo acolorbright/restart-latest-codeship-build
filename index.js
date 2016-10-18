7834f4063e21816bb57ad50599174d58ca6441a8
Philipp Bosch <hello@pb.io>
2016-10-18 14:41:20 +0200

    Switch to /:api_key/:project_id URL structure, add instructions for / route, more error handling.

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const app = require('express')();
 const request = require('request');
 
-app.all('/', (req, res) => {
-  // Check if required parameters are provided
-  if (!req.query.api_key || !req.query.project) {
-    res.status(500).send('Missing one or both required parameters: "api_key" and "project"');
-    return;
-  }
+app.get('*', (req, res) => {
+  res.send(`<!doctype html><title>Restart latest Codeship build</title><code><p><strong>USAGE</strong><br>curl -X POST https://${req.headers.host}/<mark>:codeship_api_key</mark>/<mark>:codeship_project_id</mark></p></code>`);
+});
 
+app.post('/:api_key/:project_id', (req, res) => {
   // Get all Codeship builds for this project
-  request.get(`https://codeship.com/api/v1/projects/${req.query.project}.json?api_key=${req.query.api_key}`, function (error, response, body) {
-    if (error) {
-      res.status(500).send(error);
+  request.get(`https://codeship.com/api/v1/projects/${req.params.project_id}.json?api_key=${req.params.api_key}`, (buildsError, _, buildsBody) => {
+    if (buildsError) {
+      res.status(500).send(buildsError);
     } else {
-      const data = JSON.parse(body);
+      const data = JSON.parse(buildsBody);
 
       // Find the latest build for the master branch
       const build = data.builds.find(b => b.branch === 'master');
@@ -22,12 +20,16 @@ app.all('/', (req, res) => {
         res.status(500).send('No build from master branch found.');
       } else {
         // Restart the build on Codeship
-        request.post(`https://codeship.com/api/v1/builds/${build.id}/restart.json?api_key=${req.query.api_key}`, function(error, response, body) {
-          res.send(JSON.parse(body));
+        request.post(`https://codeship.com/api/v1/builds/${build.id}/restart.json?api_key=${req.params.api_key}`, (buildError, __, buildBody) => {
+          if (buildError) {
+            res.status(500).send(buildError);
+          } else {
+            res.send(JSON.parse(buildBody));
+          }
         });
       }
     }
   });
-
 });
+
 app.listen(3333);
